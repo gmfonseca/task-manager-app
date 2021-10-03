1b531a8c129dc9c1f359cc04ccfa888a05844f03
Gabriel Fonseca <32571975+gmfonseca@users.noreply.github.com>
2021-10-03 18:37:40 -0300

    [iOS] Add feature to take picture and finish task

--- iosApp/iosApp/Views/Camera/ImagePickerView.swift
+++ iosApp/iosApp/Views/Camera/ImagePickerView.swift
@@ -10,18 +10,43 @@ import UIKit
 import SwiftUI
 
 struct ImagePickerView: UIViewControllerRepresentable {
-    
-    @Binding var selectedImage: UIImage?
+
     @Environment(\.presentationMode) var isPresented
     var sourceType: UIImagePickerController.SourceType
+    
+    let onSelectedImage: (UIImage) -> Void
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = self.sourceType
+        imagePicker.delegate = context.coordinator
         return imagePicker
     }
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self, onSelectedImage: onSelectedImage)
+    }
+}
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    var onSelectedImage: (UIImage) -> Void
+
+    init(picker: ImagePickerView, onSelectedImage: @escaping (UIImage) -> Void) {
+        self.picker = picker
+        self.onSelectedImage = onSelectedImage
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+
+        self.picker.isPresented.wrappedValue.dismiss()
+        onSelectedImage(selectedImage)
     }
 }
