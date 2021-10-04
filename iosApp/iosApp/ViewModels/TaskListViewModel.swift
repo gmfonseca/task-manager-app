4d4fc35bf582d61a7aaa75e032d79cdfa25f4065
Gabriel Fonseca <32571975+gmfonseca@users.noreply.github.com>
2021-10-03 22:06:53 -0300

    [iOS] Fix broken import and move view model

---
+++ iosApp/iosApp/ViewModels/TaskListViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  TaskListViewModel.swift
+//  iosApp
+//
+//  Created by Gabriel Fonseca on 03/10/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+class TaskListViewModel: ObservableObject {
+    
+    @Published var tasks: [Task]?
+
+    private var routineFlow: Closeable?
+
+    func startTasksRoutine() {
+        if(routineFlow == nil) {
+            routineFlow = fetchTasks()
+        }
+    }
+
+    func stopTasksRoutine() {
+        if let flow = routineFlow {
+            flow.close()
+            self.routineFlow = nil
+        }
+    }
+
+    private func fetchTasks() -> Closeable {
+        return FetchRemoteTasksRoutineUseCaseImpl().invoke()
+            .watch { result in
+                if let tasks = result!.getOrNull() as? [Task] {
+                    self.tasks = tasks
+                } else {
+                    if let e = result!.exceptionOrNull() {
+                        print("Failed: \(e)")
+                    } else {
+                        print("Unkown failure")
+                    }
+                }
+            }
+    }
+    
+    func completeTask(task: Task, image: UIImage) {
+        guard let data = image.pngData() else { return }
+        let params = CompleteTasksRoutineUseCaseParams(id: task.id, fileBytes: ImageParsersKt.toByteArray(data))
+
+        CompleteTasksRoutineUseCaseImpl().invoke(params: params).watch { result in }
+    }
+}
--- iosApp/iosApp/Views/TasksListView.swift
+++ iosApp/iosApp/Views/TasksListView.swift
@@ -27,13 +27,16 @@ struct TasksListView: View {
                             description: task.description_,
                             isCompleted: task.isCompleted
                         ),
-                        onClick: { taskCardView in
-                            if taskListViewModel.currentTaskId == nil {
-                                isImagePickerDisplay.toggle()
-                                taskListViewModel.currentTaskId = task.id
-                            }
+                        onClick: { _ in
+                            isImagePickerDisplay.toggle()
                         }
                     )
+                    .sheet(isPresented: self.$isImagePickerDisplay) {
+                        ImagePickerView(sourceType: self.sourceType) { selectedImage in
+
+                            taskListViewModel.completeTask(task: task, image: selectedImage)
+                        }
+                    }
                 }
             } else {
                 Text("No tasks available")
@@ -45,63 +48,11 @@ struct TasksListView: View {
         }
         .onAppear { taskListViewModel.startTasksRoutine() }
         .onDisappear {  taskListViewModel.stopTasksRoutine() }
-        .sheet(isPresented: self.$isImagePickerDisplay) {
-            ImagePickerView(sourceType: self.sourceType) { selectedImage in
-                taskListViewModel.completeTask(image: selectedImage)
-            }
-        }
     }
 }
 
 struct TasksList_Previews: PreviewProvider {
     static var previews: some View {
         TasksListView(taskListViewModel: TaskListViewModel())
-    }
-}
-
-class TaskListViewModel: ObservableObject {
-    
-    @Published var tasks: [Task]?
-    var currentTaskId: String? = nil
-
-    private var routineFlow: Ktor_ioCloseable?
-
-    func startTasksRoutine() {
-        if(routineFlow == nil) {
-            routineFlow = fetchTasks()
-        }
-    }
-
-    func stopTasksRoutine() {
-        if let flow = routineFlow {
-            flow.close()
-            self.routineFlow = nil
-        }
-    }
-
-    private func fetchTasks() -> Closeable {
-        return FetchRemoteTasksRoutineUseCaseImpl().invoke()
-            .watch { result in
-                if let tasks = result!.getOrNull() as? [Task] {
-                    self.tasks = tasks
-                } else {
-                    if let e = result!.exceptionOrNull() {
-                        print("Failed: \(e)")
-                    } else {
-                        print("Unkown failure")
-                    }
-                }
-            }
-    }
-    
-    func completeTask(image: UIImage) {
-        guard let taskId = currentTaskId else { return }
-        guard let data = image.pngData() else { return }
-        let params = CompleteTasksRoutineUseCaseParams(id: taskId, fileBytes: ImageKt.toByteArray(data))
-
-        currentTaskId = nil
-
-        CompleteTasksRoutineUseCaseImpl().invoke(params: params).watch { _ in
-        }
     }
 }
