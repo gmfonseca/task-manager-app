3f954a93443ecef6a1beff8854ad44bef83c26ae
Gabriel Fonseca <gmagnofonseca@gmail.com>
2021-10-24 20:37:56 -0300

    [iOS] Add filter after completing task

--- iosApp/iosApp/ViewModels/TaskListViewModel.swift
+++ iosApp/iosApp/ViewModels/TaskListViewModel.swift
@@ -45,8 +45,21 @@ class TaskListViewModel: ObservableObject {
     
     func completeTask(task: Task, image: UIImage) {
         guard let data = image.pngData() else { return }
-        let params = CompleteTasksUseCaseParams(id: task.id, fileBytes: ImageParsersKt.toByteArray(data))
+        let params = CompleteTaskUseCaseParams(id: task.id, fileBytes: ImageParsersKt.toByteArray(data))
 
-        CompleteTasksUseCaseImpl().invoke(params: params).watch { result in }
+        CompleteTaskUseCaseImpl().invoke(params: params)
+            .watch { result in
+                if result!.getOrNull() == true {
+                    self.tasks = self.tasks?.filter { it in
+                        it.id != task.id
+                    }
+                } else {
+                    if let e = result!.exceptionOrNull() {
+                        print("Failed: \(e)")
+                    } else {
+                        print("Unkown failure")
+                    }
+                }
+            }
     }
 }
