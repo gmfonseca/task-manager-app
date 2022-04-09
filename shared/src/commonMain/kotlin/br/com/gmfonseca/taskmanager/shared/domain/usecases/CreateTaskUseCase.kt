012452a5e88e2e30c3031005f3d506b60c7b1f6c
gmfonseca <32571975+gmfonseca@users.noreply.github.com>
2022-04-09 17:12:24 -0400

    [Shared] Add task return for create task use case

--- shared/src/commonMain/kotlin/br/com/gmfonseca/taskmanager/shared/domain/usecases/CreateTaskUseCase.kt
+++ shared/src/commonMain/kotlin/br/com/gmfonseca/taskmanager/shared/domain/usecases/CreateTaskUseCase.kt
@@ -4,11 +4,11 @@ import br.com.gmfonseca.taskmanager.shared.common.Result
 import br.com.gmfonseca.taskmanager.shared.common.ext.WatchableFlow
 import br.com.gmfonseca.taskmanager.shared.common.ext.watchableFlow
 import br.com.gmfonseca.taskmanager.shared.data.repositories.TaskRepositoryImpl
+import br.com.gmfonseca.taskmanager.shared.domain.entities.Task
 import br.com.gmfonseca.taskmanager.shared.domain.repositories.TaskRepository
 import kotlinx.coroutines.flow.Flow
 
-interface CreateTaskUseCase :
-    UseCase<CreateTaskUseCase.Params, Flow<Result<Boolean>>> {
+interface CreateTaskUseCase : UseCase<CreateTaskUseCase.Params, Flow<Result<Task>>> {
 
     data class Params(val title: String, val description: String) : UseCase.Params
 }
@@ -16,13 +16,13 @@ interface CreateTaskUseCase :
 class CreateTaskUseCaseImpl : CreateTaskUseCase {
     private val taskRepository: TaskRepository by lazy { TaskRepositoryImpl() }
 
-    override fun invoke(params: CreateTaskUseCase.Params): WatchableFlow<Result<Boolean>> =
+    override fun invoke(params: CreateTaskUseCase.Params): WatchableFlow<Result<Task>> =
         watchableFlow {
             val result = try {
                 val (title, description) = params
-                val task = taskRepository.createTask(title, description)
-
-                Result.success(task != null)
+                taskRepository.createTask(title, description)
+                    ?.let { Result.success(it) }
+                    ?: Result.failure(Error("Failed to create the task"))
             } catch (other: Throwable) {
                 Result.failure(other)
             }
